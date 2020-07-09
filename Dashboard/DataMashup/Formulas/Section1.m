382f05776065d69f2d5f5653a3e6c7444754019f
Vitor Marques <vitor.marques@gotodata.com.br>
2020-07-09 14:02:48 -0300

    V8 dashboard SQL

--- Dashboard/DataMashup/Formulas/Section1.m
+++ Dashboard/DataMashup/Formulas/Section1.m
@@ -1,7 +1,10 @@
 section Section1;
 
 shared Pessoas = let
-    Fonte = Sql.Database("DELL-I15-A30S", "TST_PBI"),
+    Fonte = Sql.Database("DELL-I15-A30S", Database),
     dbo_Pessoas = Fonte{[Schema="dbo",Item="Pessoas"]}[Data]
 in
     dbo_Pessoas;
+
+[ Description = "Seleção do database" ]
+shared Database = "TST_PBI" meta [IsParameterQuery=true, List={"TST_PBI", "TST_PBI_PROD"}, DefaultValue="TST_PBI", Type="Text", IsParameterQueryRequired=true];
