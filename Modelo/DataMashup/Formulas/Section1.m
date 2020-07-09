7b2491251e2505c0f08816db75a22c4691423934
Vitor Marques <vitor.marques@gotodata.com.br>
2020-07-09 12:23:32 -0300

    V5 dashboard SQL

--- Modelo/DataMashup/Formulas/Section1.m
+++ Modelo/DataMashup/Formulas/Section1.m
@@ -1,7 +1,7 @@
 section Section1;
 
-shared Tabela = let
-    Fonte = Table.FromRows(Json.Document(Binary.Decompress(Binary.FromText("i45WCsssyS9S0lEyMlWK1YlWck9MKspMzUkEiViARZxzUjNTUoF8E0sw3z8lMyc/D8g3NVaKjQUA", BinaryEncoding.Base64), Compression.Deflate)), let _t = ((type nullable text) meta [Serialized.Text = true]) in type table [Nome = _t, Idade = _t]),
-    #"Tipo Alterado" = Table.TransformColumnTypes(Fonte,{{"Nome", type text}, {"Idade", Int64.Type}})
+shared Pessoas = let
+    Fonte = Sql.Database("DELL-I15-A30S", "TST_PBI"),
+    dbo_Pessoas = Fonte{[Schema="dbo",Item="Pessoas"]}[Data]
 in
-    #"Tipo Alterado";
+    dbo_Pessoas;
