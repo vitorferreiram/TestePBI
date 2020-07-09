ef4357a073c5893a2f8b9713d8b12847a84605ff
Vitor Marques <vitor.marques@gotodata.com.br>
2020-07-09 12:34:16 -0300

    V7 dashboard SQL

--- Dashboard/DataMashup/Formulas/Section1.m
+++ Dashboard/DataMashup/Formulas/Section1.m
@@ -1,7 +1,7 @@
 section Section1;
 
 shared Pessoas = let
-    Fonte = Sql.Database("DELL-I15-A30S", "TST_PBI_PROD"),
+    Fonte = Sql.Database("DELL-I15-A30S", "TST_PBI"),
     dbo_Pessoas = Fonte{[Schema="dbo",Item="Pessoas"]}[Data]
 in
     dbo_Pessoas;
